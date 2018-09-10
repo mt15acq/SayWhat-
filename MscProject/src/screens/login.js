cd56edb261dc7e745f6d7ebffc223e0c0334aedd
mtome <mt15acq@herts.ac.uk>
2018-09-10 08:26:45 +0100

    Added LogIn Screen / Added Background and Fonts Img

--- MscProject/src/screens/login.js
+++ MscProject/src/screens/login.js
@@ -2,23 +2,49 @@ import React, { Component } from 'react';
 import { 
     ImageBackground, 
     View, 
+    Text,
     TextInput, 
     StyleSheet,
     Dimensions
 } from 'react-native';
+import CustomButton from './customButton'
 const { width, height } = Dimensions.get( 'window' );
 
 export default class login extends Component{
     render(){
         return (
-            <ImageBackground style = { styles.imageBackgroundContainer }>
+            <ImageBackground 
+                style = { styles.imageBackgroundContainer }
+                source = { require ('../../assets/img/background.png')}
+                >            
             <View style = { styles.usernameTextInputContainer }>
-                 <TextInput style = {styles.textInputFont}/>        
+                 <TextInput 
+                    style = {styles.textInput}
+                    placeholder = 'Username'
+                    placeholderTextColor = 'white'
+                    underlineColorAndroid = 'white'
+                />        
             </View>
             <View style = { styles.passwordTextInputContainer }>
-                <TextInput style = {{ width : '50%', height : height*0.1 }}/>
+                <TextInput 
+                    style = {{ width : '50%', height : height*0.1 }}
+                    placeholder = 'Password'
+                    placeholderTextColor = 'white'
+                    fontFamily =  'ARBERKLEY'
+                    underlineColorAndroid = 'white'
+                />
             </View>
-        </ImageBackground>
+            <View style = { styles.logInButtonContainer} >
+                <CustomButton
+                    style = {{width : '50%'}}
+                    text = 'LOG IN'
+                />
+                <Text 
+                    style = { styles.forgotPassword }>
+                    {'Forgot password?'}
+                </Text>
+            </View>
+            </ImageBackground>
         )
     }
 }
@@ -26,33 +52,60 @@ export default class login extends Component{
 const styles = StyleSheet.create ({
     imageBackgroundContainer : {
         flex : 1,
-        flexDirection : 'column'
+        flexDirection : 'column',
+        width :  width,
+        height : height
     },
 
     usernameTextInputContainer : {
-        flex : 1,
-        backgroundColor : 'yellow',
-        flexDirection : 'row',
-        width : width * 0.7,
-        height : height * 0.10,
         alignItems : 'flex-end',
+       /* backgroundColor : 'pink',*/
+        flex : 0.5,
+        flexDirection : 'row',
+       /* width : width * 0.7,
+        height : height * 0.10,*/
         justifyContent : 'center',
     },
 
     passwordTextInputContainer : {
-        flex : 1,
-        backgroundColor: 'green',
-        flexDirection : 'row', 
-        width : width * 0.7,
-        height : height * 0.10,
         alignItems : 'flex-start',
-        justifyContent : 'center'
+       /* backgroundColor: 'green',*/
+        flex : 0.2,
+        flexDirection : 'row', 
+      /*  height : height * 0.10,*/
+        justifyContent : 'center',
+        /*width : width * 0.7,*/
     },
 
-    textInputFont : {
+    logInButtonContainer : {
+        alignItems : 'center',
+        /*backgroundColor : 'purple',*/
+        flex : 0.2,
+        flexDirection : 'column',
+        justifyContent : 'flex-start',
+  
+    },
+
+    textInput : {
+        color : 'white',   
         fontSize : 14,
-        fontFamily :  'Ubuntu-Regular', 
+        fontFamily :  'ARBERKLEY', 
+        height : height * 0.1,    
         width : '50%',
-        height : height * 0.1
+        opacity : 0.6
+    },
+
+    forgotPassword : {
+        fontSize : 10,
+        fontFamily :  'ARBERKLEY', 
+        color : 'white',
+        marginTop : 20
+    },
+
+    button : {
+        elevation : 8,
+        color : '#004B55',
+        borderRadius: 4
+
     }
 });
